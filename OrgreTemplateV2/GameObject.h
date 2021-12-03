6d2efbe83ccb309609c8b3ef02d05b7b20cb7a38
Chris <tulip.chris@gmail.com>
2021-12-03 16:29:17 -0500

    Auto stash before checking out "origin/master"

--- OrgreTemplateV2/GameObject.h
+++ OrgreTemplateV2/GameObject.h
@@ -4,23 +4,38 @@
 #include <iostream>
 
 class Game;
-
+/// <summary>
+/// Base GameObject class. Inherits from InputObserver so that any object can respond to Inputs.
+/// </summary>
 class GameObject : public InputObserver
 {
 public:
 	GameObject(Game* gameInstance, SceneManager* scnmngr, std::string name);
 	~GameObject();
 
-	//virtual void Update(const Ogre::FrameEvent& evt) = 0;
+	/// <summary>
+	/// Virtual Update Function for game loop to call during its update.
+	/// </summary>
 	virtual void Update() = 0;
-
+	/// <summary>
+	/// Returns the pointer to the attached Node.
+	/// </summary>
+	/// <returns></returns>
 	Ogre::SceneNode* GetNode();
+	/// <summary>
+	/// Sets the node in case you need to switch the node of an object.
+	/// </summary>
+	/// <param name="newnode"></param>
 	void SetNode(Ogre::SceneNode* newnode);
-
+	/// <summary>
+	/// Virtual function that gets called when the InputObserver base class gets notified by a subject.
+	/// </summary>
+	/// <param name=""></param>
+	/// <param name=""></param>
 	virtual void ObserverUpdate(Keycode, EventType) = 0;
 
 protected:
-	Game* mGameReference;
-	Ogre::SceneNode* node;
-	Ogre::ManualObject* mesh;
+	Game* mGameReference; /**< Pointer reference to the Game instance.*/
+	Ogre::SceneNode* node; /**< Pointer to the sceneNode.*/
+	Ogre::ManualObject* mesh; /**< Pointer to the Mesh object attached to the node.*/
 };
